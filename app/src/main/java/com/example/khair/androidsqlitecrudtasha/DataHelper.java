99e25c2f2050752ff840cf992f91744ad46f3db2
tashaezileen <tashaezileen@gmail.com>
2017-10-24 23:03:40 +0800

    Test

---
+++ app/src/main/java/com/example/khair/androidsqlitecrudtasha/DataHelper.java
@@ -0,0 +1,31 @@
+package com.example.khair.androidsqlitecrudtasha;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by khair on 23/10/2017.
+ */
+
+public class DataHelper  extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "personalbiodata.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context){
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table biodata(no integer primary key, name text null, dob text null, gender text null, address text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2){ }
+
+}
